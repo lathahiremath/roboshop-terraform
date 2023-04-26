86545d257082e745286547150438cf04c397ed0e
Latha <lathahiremath03@gmail.com>
2023-04-26 23:00:58 +0530

    roboshop-terraform ,create ami instances and dns records

--- server.tf
+++ server.tf
@@ -14,6 +14,14 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "frontend-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -22,6 +30,15 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "mongodb-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -30,6 +47,14 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "catalogue-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -37,6 +62,13 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "redis-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -47,6 +79,13 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "user-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
 
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
@@ -56,6 +95,13 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "cart-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
 resource "aws_instance" "mqsql" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -64,6 +110,14 @@ resource "aws_instance" "mqsql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mqsql" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "mqsql-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mqsql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -71,6 +125,13 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "shipping-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
@@ -80,6 +141,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "rabbitmq-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
@@ -88,4 +156,11 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z03669871OW33FYX3AD4W"
+  name    = "payment-dev.latha.fun"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
