d3727215622ccd8ea336349690b0606e1ee93891
Latha <lathahiremath03@gmail.com>
2023-06-07 16:47:55 +0530

    module

---
+++ module/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = var.component_name
+  }
+}
+
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/lathahiremath/roboshop-shell",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${lookup(var.password) }"
+    ]
+  }
+}
+
+resource "aws_route53_record" "records" {
+  zone_id = "Z10325692IM5OANUYJ0F1"
+  name    = "${each.value["name"]}-dev.latha.fun.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
